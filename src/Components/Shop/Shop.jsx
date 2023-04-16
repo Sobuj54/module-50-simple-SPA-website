febb5f2c6c136fe4209aab03135d712d74811a1f
soubj <sobujahmed124@gmail.com>
2023-04-16 21:06:58 +0600

    loaded images and name

--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './Shop.css';
+import Product from '../Product/Product';
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
@@ -14,7 +15,12 @@ const Shop = () => {
     return (
         <div className='shop-container'>
             <div className="product-container">
-                <h3>Products coming here : {products.length}</h3>
+                {
+                    products.map(product =><Product 
+                         key={product.id}
+                         product={product}
+                         ></Product>)
+                }
             </div>
            <div className="cart-container">
                  <h3>order summary.</h3>
