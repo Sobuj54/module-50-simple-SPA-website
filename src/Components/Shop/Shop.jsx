aa4a0c1017c993d3a58f47e053d17187acfdab7e
soubj <sobujahmed124@gmail.com>
2023-04-18 11:36:36 +0600

    added id quantity to local storage

--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import './Shop.css';
 import Product from '../Product/Product';
 import Cart from '../Cart/Cart';
+import { addToDb } from '../../utilities/fakedb';
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
@@ -18,6 +19,7 @@ const Shop = () => {
     const handleAddToCart = (product) => {
         let newCart = [...cart, product];
         setCart(newCart);
+        addToDb(product.id);
     }
 
     return (
