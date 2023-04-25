e0ce8765cbdc7f51f572de6a7b6fbd68c7007fbf
soubj <sobujahmed124@gmail.com>
2023-04-25 22:17:18 +0600

    finished nested path

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,34 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./Components/Layout/Home";
 import Shop from "./Components/Shop/Shop";
+import Order from "./Components/Order/Order";
+import Inventory from "./Components/Inventory/Inventory";
+import Login from "./Components/Login/Login";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Shop></Shop>,
+    element: <Home></Home>,
+    children: [
+      {
+        path: "/",
+        element: <Shop></Shop>,
+      },
+      {
+        path: "order",
+        element: <Order></Order>,
+      },
+      {
+        path: "inventory",
+        element: <Inventory></Inventory>,
+      },
+      {
+        path: "login",
+        element: <Login></Login>,
+      },
+    ],
   },
 ]);
 
