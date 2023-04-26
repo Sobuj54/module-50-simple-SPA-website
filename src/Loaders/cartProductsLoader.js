9947b960fc6c2f988109abe05985e6f03e94de3a
soubj <sobujahmed124@gmail.com>
2023-04-26 12:22:44 +0600

    retrieved data from localStorage

--- src/Loaders/cartProductsLoader.js
+++ src/Loaders/cartProductsLoader.js
@@ -1,7 +1,26 @@
+import { getShoppingCart } from "../utilities/fakedb";
+
 const cartsProductsLoader = async () => {
   const loadedProducts = await fetch("products.json");
   const products = await loadedProducts.json();
-  return products;
+
+  // if your cart data is in database, then you have to use async await
+  const storedCart = getShoppingCart();
+
+  const savedCart = [];
+  for (const id in storedCart) {
+    const addedProduct = products.find((product) => product.id === id);
+    if (addedProduct) {
+      const quantity = storedCart[id];
+      addedProduct.quantity = quantity;
+      savedCart.push(addedProduct);
+    }
+  }
+  // if you need to  return two things return them as array
+  // return [savedCart , products];
+  // other way
+  // return {savedCart , products};
+  return savedCart;
 };
 
 export default cartsProductsLoader;
