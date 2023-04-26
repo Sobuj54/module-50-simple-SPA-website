e36eda51e072615134223836609d1d7d0cf8d5a2
soubj <sobujahmed124@gmail.com>
2023-04-26 18:38:30 +0600

    created succesfully working delete button

--- src/Components/Order/Order.jsx
+++ src/Components/Order/Order.jsx
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import Cart from "../Cart/Cart";
 import { useLoaderData } from "react-router-dom";
 import ReviewItem from "../ReviewItem/ReviewItem";
 import "./Order.css";
+import { removeFromDb } from "../../utilities/fakedb";
 
 const Order = () => {
-  const cart = useLoaderData();
+  const savedCart = useLoaderData();
+
+  const [cart, setCart] = useState(savedCart);
+
+  const removeFromCart = (id) => {
+    const remaining = cart.filter((product) => product.id !== id);
+    setCart(remaining);
+    removeFromDb(id);
+  };
+
   return (
     <div className="shop-container">
       <div className="review-container">
         {cart.map((product) => (
-          <ReviewItem key={product.id} product={product}></ReviewItem>
+          <ReviewItem
+            key={product.id}
+            product={product}
+            removeFromCart={removeFromCart}></ReviewItem>
         ))}
       </div>
       <div className="cart-container">
