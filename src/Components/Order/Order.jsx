509298a65abbff5bfb3db49a1c8ac939c46290e3
soubj <sobujahmed124@gmail.com>
2023-04-26 13:20:19 +0600

    loaded image in review item

--- src/Components/Order/Order.jsx
+++ src/Components/Order/Order.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import Cart from "../Cart/Cart";
 import { useLoaderData } from "react-router-dom";
+import ReviewItem from "../ReviewItem/ReviewItem";
+import "./Order.css";
 
 const Order = () => {
   const cart = useLoaderData();
   return (
     <div className="shop-container">
-      <div className="product-container">
-        <h3>orders : {cart.length}</h3>
+      <div className="review-container">
+        {cart.map((product) => (
+          <ReviewItem key={product.id} product={product}></ReviewItem>
+        ))}
       </div>
       <div className="cart-container">
         <Cart cart={cart}></Cart>
