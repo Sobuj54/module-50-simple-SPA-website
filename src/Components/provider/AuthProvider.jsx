8db047d0269cfe6958e2c828cc30daabd56b2c0a
soubj <sobujahmed124@gmail.com>
2023-05-07 20:55:35 +0600

    log in with user password and recognize

--- src/Components/provider/AuthProvider.jsx
+++ src/Components/provider/AuthProvider.jsx
@@ -1,5 +1,9 @@
 import React, { createContext, useState } from "react";
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import app from "../../firebase/firebase.config";
 
 export const AuthContext = createContext(null);
@@ -13,7 +17,11 @@ const AuthProvider = ({ children }) => {
     return createUserWithEmailAndPassword(auth, email, password);
   };
 
-  const userInfo = { createUser };
+  const logIn = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const userInfo = { createUser, logIn };
   return (
     <AuthContext.Provider value={userInfo}>{children}</AuthContext.Provider>
   );
