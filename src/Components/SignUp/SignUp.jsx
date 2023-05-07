534a3aa6fa59121859cc8c289c3bf6dc9f03877e
soubj <sobujahmed124@gmail.com>
2023-05-07 20:27:16 +0600

    created context

--- src/Components/SignUp/SignUp.jsx
+++ src/Components/SignUp/SignUp.jsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SignUp.css";
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
+  const [error, setError] = useState("");
+
+  const handleSignUp = (event) => {
+    event.preventDefault();
+
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    const confirm = form.confirm.value;
+
+    console.log(email, password, confirm);
+
+    if (password !== confirm) {
+      setError("password did not match.");
+    } else if (password.length < 6) {
+      setError("password must be 6 characters or longer.");
+    }
+  };
+
   return (
     <div className="form-container">
       <h2 className="form-title">Sign Up </h2>
-      <form>
+      <form onSubmit={handleSignUp}>
         <div className="form-control">
           <label htmlFor="email">Email</label>
           <input type="email" name="email" required />
@@ -15,11 +35,17 @@ const SignUp = () => {
           <input type="password" name="password" required />
         </div>
         <div className="form-control">
-          <label htmlFor="password">Confirm Password</label>
+          <label htmlFor="confirm">Confirm Password</label>
           <input type="password" name="confirm" required />
         </div>
         <input className="btn-submit" type="submit" value="Signup" />
       </form>
+      <p>
+        <small>
+          Already have an account? <Link to="/login">Login</Link>
+        </small>
+      </p>
+      <p className="text-error">{error}</p>
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -11,6 +11,7 @@ import Login from "./Components/Login/Login";
 import cartsProductsLoader from "./Loaders/cartProductsLoader";
 import CheckOut from "./Components/Checkout/CheckOut";
 import SignUp from "./Components/SignUp/SignUp";
+import AuthProvider from "./Components/provider/AuthProvider";
 
 const router = createBrowserRouter([
   {
@@ -48,6 +49,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </React.StrictMode>
 );
